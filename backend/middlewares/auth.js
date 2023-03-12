56af3dcd84b522f4a96499b7cd9d4ed189e664db
Vladimir Iakhnev <Yahnev91@yandex.ru>
2023-03-12 13:00:46 +0300

    Preload

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { maxAge: 3600000 * 24 * 365 });
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new UnauthorizedError('Сначала нужно авторизироваться'));
   }
